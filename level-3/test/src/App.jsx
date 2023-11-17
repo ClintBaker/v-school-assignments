362b43b8b0d61eebda40fd98ed1b37197af39f86
Clint Baker <clintjbaker@gmail.com>
2023-11-16 20:17:35 -0700

    Vite tutorial for Yabanjin

--- level-3/test/src/App.jsx
+++ level-3/test/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <>Test</>;
+  const newVar = ["B", "A", "N"];
+  return (
+    <div>
+      <h1>{newVar.map((str) => str.toLowerCase())}</h1>
+    </div>
+  );
 }
 
 export default App;
