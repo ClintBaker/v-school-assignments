98dac064890d20d6ec32e639d9d631efb249415f
Clint Baker <clintjbaker@gmail.com>
2024-01-16 12:54:25 -0700

    optimize mid level assesment

--- level-5/assesments/mid-level/sort.js
+++ level-5/assesments/mid-level/sort.js
@@ -1,13 +1,16 @@
 function sortByProperty(objects, propertyName) {
   // Check if we're comparing a value that's a num or string
-  if (isNaN(objects[0][propertyName])) {
+  if (typeof objects[0][propertyName] === "string") {
     // If string we use localeCompare
     return objects.sort((a, b) =>
       a[propertyName].localeCompare(b[propertyName])
     );
-  } else {
+  } else if (typeof objects[0][propertyName] === "number") {
     // If num we use a simple - in sort callback
     return objects.sort((a, b) => a[propertyName] - b[propertyName]);
+  } else {
+    // If it's neither a num or str we just throw an error
+    throw new Error("Unable to process request");
   }
 }
 
